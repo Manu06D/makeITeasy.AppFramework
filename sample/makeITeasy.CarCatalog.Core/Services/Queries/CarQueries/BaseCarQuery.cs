38c477f80b04b740a46d30d5777c47e33e97be2b
Manu D <edesigau@hotmail.fr>
2020-10-15 07:28:02 +0200

    Add search by name to car index page

--- sample/makeITeasy.CarCatalog.Core/Services/Queries/CarQueries/BaseCarQuery.cs
+++ sample/makeITeasy.CarCatalog.Core/Services/Queries/CarQueries/BaseCarQuery.cs
@@ -9,8 +9,9 @@ namespace makeITeasy.CarCatalog.Core.Services.Queries.CarQueries
 {
     public class BaseCarQuery : BaseQuery<Car>
     {
-        [Required]
         public long? ID { get; set; }
+
+        public string Name { get; set; }
 
         public bool? IsModernCar { get; set; }
 
@@ -21,6 +22,11 @@ namespace makeITeasy.CarCatalog.Core.Services.Queries.CarQueries
             if (ID.HasValue && ID.Value > 0)
             {
                 AddFunctionToCriteria(x => x.Id == ID);
+            }
+
+            if(!string.IsNullOrWhiteSpace(Name))
+            {
+                AddFunctionToCriteria(x => x.Name.StartsWith(Name));
             }
 
             HandleNullableBoolSearch(IsModernCar, Car.ModernCarFunction);
