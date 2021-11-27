36190255ff38d56c9a79b818453b1813bf1ba46b
Manu D <eded74@live.fr>
2021-11-27 09:05:28 +0100

    fix small issue on edit

--- makeITeasy.CarCatalog.WebApp.dotnet6/Controllers/HomeController.cs
+++ makeITeasy.CarCatalog.WebApp.dotnet6/Controllers/HomeController.cs
@@ -1,4 +1,13 @@
-﻿using makeITeasy.CarCatalog.WebApp.dotnet6.Models;
+﻿using AutoMapper;
+
+using makeITeasy.AppFramework.Core.Commands;
+using makeITeasy.AppFramework.Core.Queries;
+using makeITeasy.CarCatalog.Core.Services.Queries.BrandQueries;
+using makeITeasy.CarCatalog.Core.Services.Queries.CarQueries;
+using makeITeasy.CarCatalog.Models;
+using makeITeasy.CarCatalog.WebApp.dotnet6.Models;
+
+using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +18,62 @@ namespace makeITeasy.CarCatalog.WebApp.dotnet6.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMediator mediator, IMapper mapper)
         {
             _logger = logger;
+            _mediator = mediator;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id > 0)
+            {
+                var cars =
+                    await _mediator.Send(new GenericQueryWithProjectCommand<Car, CarEditViewModel>(
+                            new BaseCarQuery() { ID = id, Includes = new List<System.Linq.Expressions.Expression<Func<Car, object>>>() { x => x.Brand } }));
+
+                CarEditViewModel model = cars.Results.FirstOrDefault();
+
+                QueryResult<Brand> output = await _mediator.Send(new GenericQueryCommand<Brand>(new BaseBrandQuery()));
+
+                model.Brands = output.Results.ToList();
+
+                return base.PartialView(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit([FromBody] CarEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                CommandResult<Car> result = await _mediator.Send(new UpdateEntityCommand<Car>(_mapper.Map<Car>(model)));
+
+                if (result.Result == CommandState.Success)
+                {
+                    return Ok(model);
+                }
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError, "An error has occured");
+        }
+
+        public async Task<IActionResult> CarDetails(int id)
+        {
+            QueryResult<Car> result = await _mediator.Send(new GenericQueryCommand<Car>(new BaseCarQuery() { ID = id, IncludeStrings = new List<string>() { "Brand" } }));
+
+            return PartialView(result.Results.FirstOrDefault());
         }
 
         public IActionResult Privacy()
