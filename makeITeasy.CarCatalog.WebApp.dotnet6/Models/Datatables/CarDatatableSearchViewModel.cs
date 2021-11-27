5b83e17bb8717f437fe5657c455a98cc3194c0e7
Manu D <eded74@live.fr>
2021-11-27 08:57:46 +0100

    Update dotnet 6 web project

---
+++ makeITeasy.CarCatalog.WebApp.dotnet6/Models/Datatables/CarDatatableSearchViewModel.cs
@@ -0,0 +1,8 @@
+﻿using makeITeasy.CarCatalog.Core.Services.Queries.CarQueries;
+
+namespace makeITeasy.CarCatalog.WebApp.dotnet6.Models.Datatables
+{
+    public class CarDatatableSearchViewModel : BaseCarQuery
+    {
+    }
+}
--- makeITeasy.CarCatalog.WebApp.dotnet6/Program.cs
+++ makeITeasy.CarCatalog.WebApp.dotnet6/Program.cs
@@ -1,6 +1,25 @@
 using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection;
+
+using FluentValidation;
+
 using makeITeasy.AppFramework.Core.Helpers;
+using makeITeasy.AppFramework.Core.Infrastructure.Autofac;
+using makeITeasy.AppFramework.Core.Interfaces;
 using makeITeasy.AppFramework.Models;
+using makeITeasy.AppFramework.Web.Filters;
+using makeITeasy.AppFramework.Web.Helpers;
+using makeITeasy.CarCatalog.Core.Ports;
+using makeITeasy.CarCatalog.Core.Services.Interfaces;
+using makeITeasy.CarCatalog.Core.Services;
+using makeITeasy.CarCatalog.Infrastructure.Data;
+using makeITeasy.CarCatalog.Infrastructure.Persistence;
+using makeITeasy.CarCatalog.Infrastructure.Repositories;
+using makeITeasy.CarCatalog.Models;
+
+using Microsoft.EntityFrameworkCore;
 
 using Serilog;
 
@@ -10,22 +29,52 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, config) => config.WriteTo.Console());
 
+Assembly[] assembliesToScan = new Assembly[]
+    {
+        makeITeasy.AppFramework.Core.AppFrameworkCore.Assembly,
+        makeITeasy.CarCatalog.Core.CarCatalogCore.Assembly,
+        AppFrameworkModels.Assembly
+    };
 
-Assembly[] assembliesToScan = new Assembly[]{
-    makeITeasy.AppFramework.Core.AppFrameworkCore.Assembly,
-    makeITeasy.CarCatalog.Core.CarCatalogCore.Assembly,
-    AppFrameworkModels.Assembly
-};
 
-builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+builder.Services.AddControllersWithViews();
+builder.Services.AddOptions();
+builder.Services.AddScoped<DatatableExceptionFilter>();
+builder.Services.AddDbContextFactory<CarCatalogContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnectionString"))
+
+    .EnableDetailedErrors()
+    .EnableSensitiveDataLogging()
+    ;
+    options.AddInterceptors(new DatabaseInterceptor());
+});
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(
+    builder =>
     {
         builder.RegisterModule(new RegisterAutofacModule() { Assemblies = assembliesToScan });
+        builder.RegisterAutoMapper(assembliesToScan);
+        builder.RegisterMediatR(assembliesToScan);
+
+        builder.RegisterType<CarCatalogContext>();
+
+        builder.RegisterGeneric(typeof(TransactionCarCatalogRepository<>)).As(typeof(IAsyncRepository<>)).InstancePerLifetimeScope()
+                .PropertiesAutowired()
+                .OnActivated(args => AutofacHelper.InjectProperties(args.Context, args.Instance, true));
+
+        builder.RegisterAssemblyTypes(CarCatalogModels.Assembly).Where(t => t.IsClosedTypeOf(typeof(IValidator<>))).AsImplementedInterfaces();
+
+        builder.RegisterType<AutofacValidatorFactory>().As<IValidatorFactory>().SingleInstance();
+
+        ////specific service/repository
+        builder.RegisterType<CarService>().As<ICarService>();
+        builder.RegisterType<CarRepository>().As<ICarRepository>();
     }
 );
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddOptions();
+DatatableHelpers.RegisterDatatableService(builder.Services);
 
 var app = builder.Build();
 
