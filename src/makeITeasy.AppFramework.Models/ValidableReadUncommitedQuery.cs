b1cdd252334b9ee5e81f4006c680e23661b1eedb
Smekta06 <alexandre.milanesio@gmail.com>
2022-05-19 13:56:37 +0200

    Add ReadUncommited and ValidableReadUncommitedQuery

---
+++ src/makeITeasy.AppFramework.Models/ValidableReadUncommitedQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace makeITeasy.AppFramework.Models
+{
+    public abstract class ValidableReadUncommitedQuery<T> : ReadUncommitedQuery<T> where T : IBaseEntity
+    {
+        public abstract bool IsValid { get; }
+
+        public override void BuildQuery()
+        {
+            if (!IsValid)
+            {
+                string errorMessage = $"The query {this.GetType().Name} is not valid, the request was not send to the DB without filters";
+                throw new InvalidQueryException(errorMessage);
+            }
+
+            DoBuildQuery();
+        }
+
+        public abstract void DoBuildQuery();
+    }
+}
