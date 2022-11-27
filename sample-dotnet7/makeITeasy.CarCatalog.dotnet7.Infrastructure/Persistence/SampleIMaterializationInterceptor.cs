7e8e12cedd8350133b2de0279623aaee13e13cfa
Manu D <eded74@live.fr>
2022-11-27 10:26:23 +0100

    Add sample IMaterializationInterceptor to dotnet  project

---
+++ sample-dotnet7/makeITeasy.CarCatalog.dotnet7.Infrastructure/Persistence/SampleIMaterializationInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using makeITeasy.CarCatalog.dotnet7.Models;
+
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace makeITeasy.CarCatalog.dotnet7.Infrastructure.Persistence
+{
+    public class SampleIMaterializationInterceptor : IMaterializationInterceptor
+    {
+        /// <summary>
+        /// Has to be registered with AddInterceptors
+        /// Will be instanciated every time an entity will be created
+        /// </summary>
+        /// <param name="materializationData"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public object InitializedInstance(MaterializationInterceptionData materializationData, object entity)
+        {
+            if(entity is Car car)
+            {
+                //do stuff
+            }
+
+            return entity;
+        }
+    }
+}
