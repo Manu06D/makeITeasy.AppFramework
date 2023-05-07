51144c19261ebe3fc18d53801e7eb18316066f2e
Manu D <eded74@live.fr>
2023-05-07 14:33:19 +0200

    Add Moq test

--- sample-dotnet7/makeITeasy.CarCatalog.dotnet7.Core/Services/BrandService.cs
+++ sample-dotnet7/makeITeasy.CarCatalog.dotnet7.Core/Services/BrandService.cs
@@ -1,10 +1,18 @@
 ﻿using makeITeasy.AppFramework.Core.Services;
 using makeITeasy.CarCatalog.dotnet7.Models;
 using makeITeasy.CarCatalog.dotnet7.Core.Services.Interfaces;
+using FluentValidation;
+using makeITeasy.AppFramework.Core.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace makeITeasy.CarCatalog.dotnet7.Core.Services
 {
     public class BrandService : BaseEntityService<Brand>, IBrandService
     {
+        public BrandService(IAsyncRepository<Brand> entityRepository, ILogger<BaseEntityService<Brand>> logger, IValidator<Brand>? validator = null) 
+            : base(entityRepository, logger, validator)
+        {
+            
+        }
     }
 }
---
+++ sample-dotnet7/makeITeasy.CarCatalog.dotnet7.Tests/MoqSubClasses_Tests.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using Autofac.Extras.Moq;
+
+using FluentAssertions;
+
+using makeITeasy.AppFramework.Core.Interfaces;
+using makeITeasy.CarCatalog.dotnet7.Core.Services;
+using makeITeasy.CarCatalog.dotnet7.Core.Services.Interfaces;
+using makeITeasy.CarCatalog.dotnet7.Models;
+
+using Moq;
+
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace makeITeasy.CarCatalog.dotnet7.Tests
+{
+    public class MoqSubClasses_Tests
+    {
+        [Fact]
+        public async Task XX()
+        {
+            const string brandName = "MyBrand";
+
+            Mock<IAsyncRepository<Brand>> repositoryMock = new() { };
+            repositoryMock.Setup(x => x.AddAsync(It.IsAny<Brand>(), true)).ReturnsAsync(new Brand() { Name = brandName });
+            repositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Brand>(), true)).CallBase();
+
+            using (AutoMock autoMock = AutoMock.GetLoose(cfg =>
+            {
+                cfg.RegisterModule<ServiceRegistrationAutofacModule>();
+                cfg.RegisterMock(repositoryMock);
+            }))
+            {
+                //HACK : IValidator is not working with AutoMock
+                IBrandService brandService = new BrandService(autoMock.Create<IAsyncRepository<Brand>>(), null);
+
+                var xx =  await brandService.CreateAsync(new Brand() { Name = "XXX"});
+
+                await brandService.DeleteAsync(new Brand());
+
+                xx.Entity.Name.Should().Be(brandName);
+            }
+        }
+    }
+}
