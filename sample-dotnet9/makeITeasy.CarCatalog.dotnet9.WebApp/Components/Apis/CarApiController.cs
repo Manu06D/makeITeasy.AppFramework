2fc8f23d0540aca502993e9c3696c05a13ea604b
Manu D. <eded74@live.fr>
2024-12-29 08:05:56 +0100

    update packages

--- sample-dotnet9/makeITeasy.CarCatalog.dotnet9.WebApp/Components/Apis/CarApiController.cs
+++ sample-dotnet9/makeITeasy.CarCatalog.dotnet9.WebApp/Components/Apis/CarApiController.cs
@@ -1,5 +1,6 @@
 ﻿using makeITeasy.AppFramework.Core.Interfaces;
 using makeITeasy.AppFramework.Core.Queries;
+using makeITeasy.CarCatalog.dotnet9.Core.Services.Interfaces;
 using makeITeasy.CarCatalog.dotnet9.Core.Services.Queries.CarQueries;
 using makeITeasy.CarCatalog.dotnet9.Models;
 
@@ -11,7 +12,7 @@ namespace makeITeasy.CarCatalog.dotnet9.WebApp.Components.Apis
 {
     [Route("api/Car")]
     [ApiController]
-    public class CarApiController(IMediator mediator)
+    public class CarApiController(IMediator mediator, ICarService carService)
     {
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(CarResponse), StatusCodes.Status200OK)]
@@ -19,6 +20,14 @@ namespace makeITeasy.CarCatalog.dotnet9.WebApp.Components.Apis
         public async Task<CarResponse> GetCarById(int id)
         {
             return await mediator.Send(new GenericFindUniqueWithProjectCommand<Car, CarResponse>(new BaseCarQuery() { ID = id }));
+        }
+
+        [HttpGet("/update")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<int> UpdateAllCar(int id)
+        {
+            return await carService.UpdateRangeAsync(x => x.Id > 0, x => new Car { LastModificationDate = DateTime.Now });
         }
 
         public class CarResponse : IMapFrom<Car>
